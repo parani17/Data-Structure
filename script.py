e4266d005df7b5a584cb86b0a509db20ed5d214d
Paranidharan <paranidharan.r.2024.ece@rajalakshmi.edu.in>
2025-06-13 18:51:36 +0530

    Updated README.md

--- script.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for i in range(1, 8):
-    print("\n### Week {}".format(i))
-    for j, k in enumerate(os.listdir("Week {}".format(i))):
-        print("[REC_DS using C_Week_{}_COD_Question {}](Week%20{}/0{}.pdf)".format(i, j + 1, i, j + 1))
